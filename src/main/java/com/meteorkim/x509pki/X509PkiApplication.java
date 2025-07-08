05e752ee1a969ab558938d5b03df6799c90f3f39
ysmac <ivneld@naver.com>
2025-07-08 22:46:22 +0900

    Initial Commit

---
+++ src/main/java/com/meteorkim/x509pki/X509PkiApplication.java
@@ -0,0 +1,13 @@
+package com.meteorkim.x509pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X509PkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X509PkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meteorkim/x509pki/X509PkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteorkim.x509pki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X509PkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
